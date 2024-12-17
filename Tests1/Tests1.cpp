b6b23f3acbf8cbd87da71805c42fac893687c46a
PylaevaSvetlana <pylaeva.sweta@yandex.ru>
2024-12-18 00:07:13 +0300

    Добавьте файлы проекта.

---
+++ Tests1/Tests1.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Tests1
+{
+	TEST_CLASS(Tests1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
