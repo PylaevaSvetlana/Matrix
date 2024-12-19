152cd14dc0ad902886c374afa9914b07e1fd4b63
PylaevaSvetlana <pylaeva.sweta@yandex.ru>
2024-12-20 00:34:45 +0300

    Матрица

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include "..\Solver\Matrix.h"
+#include "..\Solver\Exercise.h"
 
 using namespace miit::algebra;
 int main()
 {
-	Matrix<int> matrix(4, 5);
-	std::cout << matrix << "\n";
-	matrix.FillRandom(-9, 9);
-	std::cout << "\n" << matrix << "\n";
-	matrix.FillIStream(std::cin);
-	std::cout << "\n" << matrix << "\n";
+	Matrix<int> matrix1(4, 5);
+	std::cout << matrix1 << "\n";
+	matrix1.FillRandom(-9, 9);
+	std::cout << "\n" << matrix1 << "\n";
+
+
+	Exercise solver(matrix1);
+	std::cout << "\n" << solver.task1();
 	return 0;
 }
