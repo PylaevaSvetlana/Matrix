2a9dd59d757c3420191074839acf58af1a831db0
PylaevaSvetlana <pylaeva.sweta@yandex.ru>
2024-12-19 22:01:54 +0300

    Матрица

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -1,6 +1,14 @@
-#include<iostream>
+#include <iostream>
+#include "..\Solver\Matrix.h"
 
+using namespace miit::algebra;
 int main()
 {
+	Matrix<int> matrix(4, 5);
+	std::cout << matrix << "\n";
+	matrix.FillRandom(-9, 9);
+	std::cout << "\n" << matrix << "\n";
+	matrix.FillIStream(std::cin);
+	std::cout << "\n" << matrix << "\n";
 	return 0;
 }
